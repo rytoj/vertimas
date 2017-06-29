cd1a408298a85b7c6da42851b333aea8071e4689
rytoj <you@example.com>
2017-06-29 23:41:39 +0300

    Innitial commit

---
+++ gui_handler.py
@@ -0,0 +1,118 @@
+from scrape_handler import *
+
+try:
+	# for Python 2
+	import Tkinter as tk
+	import tkMessageBox as tmb
+except ImportError:
+	# for Python 3
+	import tkinter as tk
+	import tkinter.messagebox as tmb
+
+
+class GuiHandler(object):
+	"""
+	GUI Handler class implementation.
+	"""
+
+	def __init__(self):
+		self.program_name = 'Žodžių reikšmė'
+		self.root = tk.Tk()
+		self.root.columnconfigure(0, weight=1)
+		self.root.columnconfigure(1, weight=1)
+		self.root.rowconfigure(1, weight=1)
+		self.root.rowconfigure(2, weight=1)
+
+	def start_deployment_in_gui(self):
+		"""
+		Starts deployment in GUI window.
+		"""
+		self.root.resizable(width=True, height=True)
+		self.root.focus_set()
+
+		self.__center_window()
+		self.root.wm_title(self.program_name)
+		self.__create_buttons_and_labels()
+		self.__create_textfields()
+		self.root.mainloop()
+
+	def __create_textfields(self):
+		self.output_text_lt1 = tk.Text(self.root, width=60, height=10)
+		self.output_text_lt1.grid(row=1, column=0, sticky="wens")
+		self.output_text_lt1.bind("<Up>", self.__get_selected_text)
+
+		self.output_text_en1 = tk.Text(self.root, width=60, height=10)
+		self.output_text_en1.grid(row=1, column=1, sticky="wens")
+		self.output_text_lt2 = tk.Text(self.root, width=60, height=10)
+		self.output_text_lt2.grid(row=2, column=0, sticky="wens")
+		self.output_text_en2 = tk.Text(self.root, width=60, height=10)
+		self.output_text_en2.grid(row=2, column=1, sticky="wens")
+
+	def __create_buttons_and_labels(self):
+		tk.Button(self.root, command=self.__search, text='Search').grid(
+			row=0,
+			column=1,
+			sticky="wens",
+			pady=5)
+		self.function_field_1 = tk.Entry(self.root)
+		self.function_field_1.grid(row=0, column=0, sticky="we")
+		self.function_field_1.insert('0', "labas")
+
+	def __post_output_log(self):
+		self.output_text_lt1.configure(state="normal")
+		self.output_text_lt1.insert('1.0', "test" + "\n")
+		self.output_text_lt1.configure(state="disabled")
+
+	def __search(self):
+		"""
+		Fill text fields
+		:return:
+		"""
+
+		# Lithuanian
+		entered_value = self.function_field_1.get()
+		synonyms_lt = get_synonims(entered_value)
+		if synonyms_lt:
+			self.output_text_lt1.insert('1.0', synonyms_lt + "\n\n")
+
+			entymology_lt = get_lt_word_etymology(strip_lt(entered_value))
+			self.output_text_lt2.insert('1.0', entymology_lt + "\n\n")
+
+			# English
+			translate_to_en = translate_from_lt_english(entered_value)
+			self.output_text_en1.insert('1.0', translate_to_en + "\n\n")
+			etymology = get_en_word_etymology(translate_to_en)
+			self.output_text_en2.insert('1.0', etymology + "\n\n")
+
+
+
+
+	def __center_window(self):
+		"""
+		Centers the window of tkinter.
+		"""
+		w = 750  # width for the Tk root
+		h = 500  # heighetymologyt for the Tk root
+
+		# get screen width and height
+		ws = self.root.winfo_screenwidth()  # width of the screen
+		hs = self.root.winfo_screenheight()  # height of the screen
+
+		# calculate x and y coordinates for the Tk root window
+		x = (ws / 2) - (w / 2)
+		y = (hs / 2) - (h / 2)
+
+		# set the dimensions of the screen and where it is placed
+		self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+	def __get_selected_text(self, event):
+		selected = self.output_text_lt1.get(tk.SEL_FIRST, tk.SEL_LAST)
+		LOGGER.info(selected)
+
+		self.function_field_1.delete(0, tk.END)
+		self.function_field_1.insert(0, selected)
+
+
+if __name__ == "__main__":
+	gui_handler = GuiHandler()
+	gui_handler.start_deployment_in_gui()
---
+++ scrape_handler.py
@@ -0,0 +1,187 @@
+import urllib
+from googletrans import Translator
+import logging
+from bs4 import BeautifulSoup
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %a %H:%M:%S')
+LOGGER = logging.getLogger(__name__)
+
+
+def make_soup(url):
+	req = urllib.request.Request(
+		url,
+		data=None,
+		headers={
+			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+		}
+	)
+	f = urllib.request.urlopen(req)
+	soupdata = BeautifulSoup(f, "html.parser")
+	return soupdata
+
+
+def srape_sinonimai(search_string, custom_string=""):
+	"""
+	Scrape http://sinonimas.lt
+	:param search_string:
+	:param custom_string:
+	:return:
+	"""
+	first_letter = search_string[0]
+	words = []
+	link = "http://sinonimas.lt/lt/sinonimai/" + first_letter + custom_string + "/" + search_string
+	soup = make_soup(link)
+	for x in soup.find("ul", class_="list-inline list-unstyled wordList"):
+		try:
+			word = x.a.text
+			word_href = x.a['href']
+			words.append((word, word_href))
+		except AttributeError:
+			pass
+	logging.debug(words)
+
+	return words
+
+
+def strip_lt(word_to_parse):
+	"""
+	Strips lt letters and repleces with counterparts like ą to ą
+	:param word_to_parse:
+	:param w:
+	:return:
+	"""
+	ltd = {
+		"ą": "a",
+		"č": "c",
+		"ę": "e",
+		"ė": "e",
+		"į": "i",
+		"š": "s",
+		"ų": "u",
+		"ū": "u",
+		"ž": "z"
+	}
+	w = ""
+	for letter in word_to_parse.lower():
+		remember = 0
+		for key in ltd:
+			if letter == key:
+				w += ltd[key]
+				remember = 1
+		if remember != 1:
+			w += letter
+	return w
+
+
+def get_word(word):
+	"""
+	Process words from srape_sinonimai
+	:param word:
+	:return:
+	"""
+	try:
+		fweb_synonyms = srape_sinonimai(word)
+	except TypeError as err:
+		logging.debug(err)
+		logging.info("Found nothing, trying different link")
+		try:
+			fweb_synonyms = srape_sinonimai(word, custom_string="-1")
+		except TypeError:
+			LOGGER.info("No word found")
+			return None
+		return fweb_synonyms
+	return fweb_synonyms
+
+
+def get_en_word_etymology(search_word):
+	#:TODO Fix etymology
+	"""
+	Scrape words from www.etymonline.com
+	:param search_word:
+	:return:
+	"""
+	link = "http://www.etymonline.com/index.php?allowed_in_frame=0&search=" + search_word
+	soup = make_soup(link)
+	result = ""
+	for index, element in enumerate(soup.find("dl")):
+		if index == 4: break
+		if len(element) > 1:
+			if len(element) < 40:
+				print("")
+			print(element.text)
+			result += element.text + "\n"
+	return result
+
+
+def get_lt_word_etymology(search_word):
+	"""
+	Scrape words from http://etimologija.baltnexus.lt/?w=%search_word%
+	:param search_word:
+	:return:
+	"""
+	link = "http://etimologija.baltnexus.lt/?w=" + search_word
+	soup = make_soup(link)
+	result = ""
+	for index, element in enumerate(soup.find_all("dl")):
+		print(element)
+		try:
+			antraste = element.find("dd", {"class": "title"}).text
+		except:
+			antraste = ""
+		try:
+			reiksme = element.find("dd", {"class": "meaning"}).text
+		except:
+			reiksme = ""
+		try:
+			straipsnelis = element.find("dd", {"class": "description"}).text
+		except:
+			straipsnelis = ""
+		result += "Antraštė: {}\nReikšmė: {}\nStraipsnelis: {}\n".format(antraste, reiksme, straipsnelis)
+	return result
+
+
+def translate_from_lt_english(word):
+	"""
+	Translate from lithuanian to english
+	:param word: input work lt
+	:return: output work en
+	"""
+	translator = Translator()
+	detect = translator.translate(word, src="lt", dest="en")
+	return detect.text
+
+
+def get_synonims(word):
+	"""
+	Get only synonims, without links
+	:param word:
+	:param result:
+	:return:
+	"""
+	result = ""
+	synonyms_lt = get_word(strip_lt(word))
+	LOGGER.debug(synonyms_lt)
+	for synonym, link in synonyms_lt:
+		result += synonym + ","
+	return result
+
+
+def _run_as_standalone():
+	word = "mintis"
+	print(word)
+
+	# print(get_synonims(word))
+	# lt_to_en = translate_from_lt_english(word)
+	# print(lt_to_en)
+
+	# print()
+	# print("Vertimas: {}".format(detect.text))
+	#
+	# print(get_en_word_etymology(lt_to_en))
+	get_lt_word_etymology("labas")
+
+
+if __name__ == "__main__":
+	_run_as_standalone()
